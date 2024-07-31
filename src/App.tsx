2f8b771db35184fbf7861ea3467edcbeb8c543df
Luan Miqueias <luanmiqueias92@gmail.com>
2024-07-30 23:52:03 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+
+function App() {
+	// const [count, setCount] = useState(0);
+	return (
+		<div className="bg-[#1b212b] w-full h-full flex gap-4 p-10">
+			<Button>Click me</Button>
+		</div>
+	);
+}
+
+export default App;
